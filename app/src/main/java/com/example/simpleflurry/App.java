81c493d83a3d3adaf06f0ca1d9bed5eaf3a0f4b8
Evin1- <edwin.hdz.12.0@gmail.com>
2016-05-05 00:11:01 -0400

    Create custom Application class

---
+++ app/src/main/java/com/example/simpleflurry/App.java
@@ -0,0 +1,19 @@
+package com.example.simpleflurry;
+
+import android.app.Application;
+import android.util.Log;
+
+import com.flurry.android.FlurryAgent;
+
+/**
+ * Created by evin on 5/4/16.
+ */
+public class App extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        FlurryAgent.setLogEnabled(true);
+        FlurryAgent.init(this, getString(R.string.flurry_key));
+    }
+}
